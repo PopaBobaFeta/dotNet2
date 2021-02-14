4595391f9aa73ecf4300b7a85f708c0094d4a200
PopaBobaFeta <Iskand00@gmail.com>
2021-02-15 03:14:00 +0600

    Initial commit

---
+++ dotNet2/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using dotNet2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet2.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ dotNet2/Migrations/20210214205132_Migration2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotNet2.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movie");
+        }
+    }
+}
